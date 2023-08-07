af979d8c2a9e0471bc9fccd0348d7a37c76074ea
Sonja Joubert <sonjacb@gmail.com>
2023-08-07 17:31:58 +0200

    Worked on code

--- script.js
+++ script.js
@@ -6,3 +6,41 @@ let currentGuess = [];
 let nextLetter = 0;
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
 console.log(rightGuessString)
+
+function initBoard(){
+  let board = document.getElementById('game-board')
+
+  for(let i = 0; i < NUMBER_OF_GUESSES; i++){
+    let row = document.createElement('div')
+    row.className = 'letter-row'
+
+    for(let j = 0; j < 5; j++){
+      let box = document.createElement('div')
+      box.className = 'letter-box'
+      row.appendChild(box)
+    }
+    board.appendChild(row)
+  }
+}
+initBoard()
+
+document.addEventListener('keyup', (e) => {
+  if(guessesRemaining === 0){
+    return
+  }
+  let pressedKey = String(e.key)
+  if(pressedKey === 'Backspace' && nextLetter !== 0){
+    deleteLetter()
+    return
+  }
+  if(pressedKey === 'Enter'){
+    checkGuess()
+    return
+  }
+  let found = pressedKey.match(/[a-z]/gi)
+  if(!found || found.length > 1){
+    return
+  }else {
+    insertLetter(pressedKey)
+  }
+})
